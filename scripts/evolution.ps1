08d9c9576b75e952d46efb04a39c25fc7623d51e
Miitto <wpbickerton@gmail.com>
2025-10-16 10:42:38 +0100

    feat: Speed up Evolution Demo

--- scripts/evolution.ps1
+++ scripts/evolution.ps1
@@ -1,2 +1,2 @@
 echo "========== Running Evolution =========="
-./x64/Release/IteratedPrisonersDilema.exe --rounds 50 --epsilon 0.0 --evolve --strategies ALLC,ALLD,TFT,GRIM,PAVLOV,CONTRITE,PROBER  --save ./csv/evolution.csv --format csv
+./x64/Release/IteratedPrisonersDilema.exe --rounds 50 --epsilon 0.0 --population 50 --generations 25 --evolve --strategies ALLC,ALLD,TFT,GRIM,PAVLOV,CONTRITE,PROBER  --save ./csv/evolution.csv --format csv
--- scripts/scb.ps1
+++ scripts/scb.ps1
@@ -1,2 +1,2 @@
 echo "========== Running SCB Evolution =========="
-./x64/Release/IteratedPrisonersDilema.exe --rounds 50 --epsilon 0.0 --evolve --enable-budget --strategies ALLC,ALLD,TFT,GRIM,PAVLOV,CONTRITE,PROBER  --save ./csv/scb.csv --format csv
+./x64/Release/IteratedPrisonersDilema.exe --rounds 50 --epsilon 0.0 --evolve --population 50 --generations 25 --enable-budget --strategies ALLC,ALLD,TFT,GRIM,PAVLOV,CONTRITE,PROBER  --save ./csv/scb.csv --format csv
